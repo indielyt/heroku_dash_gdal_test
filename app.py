6ac00c8e96b6b2932bf55745c94f327babd94c4a
indielyt <aragonecologic@gmail.com>
2019-01-16 12:59:31 -0700

    test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import geopandas as gpd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-# data source
+# data sources
 shp = 'shp/planimetrics_2016_centerline_trails.shp'
 df = gpd.read_file(shp)
 columns = list(df.columns.values)
